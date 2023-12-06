0351ae732b25ebc2695990cde83ea27fe91c4b00
Ming <Ming@qq.com>
2023-12-06 19:57:02 +0800

    增加网关模块 gateway

---
+++ gateway/src/main/java/com/ming/train/gateway/config/GateWayApplication.java
@@ -0,0 +1,23 @@
+package com.ming.train.gateway.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+/**
+ * @author clownMing
+ */
+@SpringBootApplication
+public class GateWayApplication {
+
+    private static final Logger LOG = LoggerFactory.getLogger(GateWayApplication.class.getName());
+
+    public static void main(String[] args) {
+        SpringApplication springApplication = new SpringApplication(GateWayApplication.class);
+        Environment environment = springApplication.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("gateway地址：\thttp://127.0.0.1:{}", environment.getProperty("server.port"));
+    }
+}
