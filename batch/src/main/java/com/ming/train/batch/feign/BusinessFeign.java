91beae2fcbe8c6a5390e383d01b6f71e460b07d2
Ming <Ming@qq.com>
2023-12-11 20:57:45 +0800

    修改bug，feign远程调用@PathVariable要写value值

--- batch/src/main/java/com/ming/train/batch/feign/BusinessFeign.java
+++ batch/src/main/java/com/ming/train/batch/feign/BusinessFeign.java
@@ -12,10 +12,10 @@ import java.util.Date;
  * @author clownMing
  */
 //@FeignClient("business")
-@FeignClient(name = "business", url = "http://127.0.0.1:8002/business")
+@FeignClient(name = "business", url = "http://localhost:8002/business")
 public interface BusinessFeign {
 
 
     @GetMapping("/admin/daily-train/gen-daily/{date}")
-    CommonResp<Object> genDaily(@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") Date date);
+    CommonResp<Object> genDaily(@PathVariable("date") @DateTimeFormat(pattern = "yyyy-MM-dd") Date date);
 }
