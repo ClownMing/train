e56177d9cc757a7ca723c3ad86a312dfb3849489
Ming <Ming@qq.com>
2023-12-12 19:55:10 +0800

    保存确认订单表，状态初始

--- business/src/main/java/com/ming/train/business/service/ConfirmOrderService.java
+++ business/src/main/java/com/ming/train/business/service/ConfirmOrderService.java
@@ -304,5 +304,37 @@ public class ConfirmOrderService {
         return confirmOrderMapper.updateByExampleSelective(confirmOrder, confirmOrderExample);
     }
 
+    public void doConfirm(ConfirmOrderDoReq req) {
+        //省略业务数据校验，如：车次是否存在，余票是否存在，车次是否在有效期内，tickets条数>0，同乘客同车次是否已买过
 
+        // 保存确认订单表，状态初始
+        DateTime now = DateTime.now();
+        ConfirmOrder confirmOrder = new ConfirmOrder();
+        confirmOrder.setId(SnowUtil.getSnowflakeNextId());
+        confirmOrder.setMemberId(LoginMemberContext.getId());
+        confirmOrder.setDate(req.getDate());
+        confirmOrder.setTrainCode(req.getTrainCode());
+        confirmOrder.setStart(req.getStart());
+        confirmOrder.setEnd(req.getEnd());
+        confirmOrder.setDailyTrainTicketId(req.getDailyTrainTicketId());
+        confirmOrder.setStatus(ConfirmOrderStatusEnum.INIT.getCode());
+        confirmOrder.setCreateTime(now);
+        confirmOrder.setUpdateTime(now);
+        confirmOrder.setTickets(JSON.toJSONString(req.getTickets()));
+        confirmOrderMapper.insert(confirmOrder);
+        // 查出余票记录，需要得到真实的库存
+
+        // 扣减余票数量，并判断余票是否足够(这里是预扣减，在Java类里扣减，不能直接更新到数据库)
+
+        // 选座
+        // 一个车厢一个车厢的获取座位数据
+
+        // 挑选符合条件的作为，如果这个车厢不满足，则进入下个车厢(多个选座应该在同一个车厢)
+
+        // 选中座位后事务处理：
+        // 座位表修改售卖情况sell
+        // 余票详情表修改余票
+        // 为会员增加购票记录
+        // 更新确认订单为成功
+    }
 }
