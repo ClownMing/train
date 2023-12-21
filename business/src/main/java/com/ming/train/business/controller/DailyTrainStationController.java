e696f55f7b597e478b8e144b3bf98254ddfe0ff1
Ming <Ming@qq.com>
2023-12-21 15:48:49 +0800

    余票查询页面增加显示车站信息

---
+++ business/src/main/java/com/ming/train/business/controller/DailyTrainStationController.java
@@ -0,0 +1,32 @@
+package com.ming.train.business.controller;
+
+import com.ming.train.business.req.DailyTrainStationQueryAllReq;
+import com.ming.train.business.resp.DailyTrainStationQueryResp;
+import com.ming.train.business.service.DailyTrainStationService;
+import com.ming.train.common.resp.CommonResp;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author clownMing
+ */
+
+@RestController
+@RequestMapping("/daily-train-station")
+public class DailyTrainStationController {
+
+    @Autowired
+    private DailyTrainStationService dailyTrainStationService;
+
+    @GetMapping("/query-by-train-code")
+    public CommonResp<List<DailyTrainStationQueryResp>> queryByTrain(@Valid DailyTrainStationQueryAllReq req) {
+        List<DailyTrainStationQueryResp> list = dailyTrainStationService.queryByTrain(req.getDate(), req.getTrainCode());
+        return new CommonResp<>(list);
+    }
+
+}
